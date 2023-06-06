c789ed627596f93a094c6af800925cb0dee8e56d
HENOK MEKONNEN <Henokme@ethiopianairlines.com>
2023-06-07 02:29:23 +0300

    fixed all change requirement issues

--- js/index.js
+++ js/index.js
@@ -1,52 +1,76 @@
-import displayDate from '../modules/dateDisplay.js';
-import displayBooks from '../modules/displayBooks.js';
-import addBooks from '../modules/addBooks.js';
-import showSection from '../modules/showSection.js';
+import displayDate from "../modules/dateDisplay.js";
+import addBooks from "../modules/addBooks.js";
+import showSection from "../modules/showSection.js";
 
 class BookList {
-  constructor() {
-    this.bookList = document.getElementById('book-list');
-    this.inputTitle = document.getElementById('title');
-    this.inputAuthor = document.getElementById('author');
-    this.inputButton = document.getElementById('add-button');
-    this.listButton = document.getElementById('contact-section');
-    this.newButton = document.getElementById('form-section');
-    this.books = JSON.parse(localStorage.getItem('book')) || [];
-    this.inputButton.addEventListener('click', (event) => this.addBooks(event));
-    this.displayBooks();
+    constructor() {
+        this.bookList = document.getElementById("book-list");
+        this.inputTitle = document.getElementById("title");
+        this.inputAuthor = document.getElementById("author");
+        this.inputButton = document.getElementById("add-button");
+        this.listButton = document.getElementById("contact-section");
+        this.newButton = document.getElementById("form-section");
+        this.books = JSON.parse(localStorage.getItem("book")) || [];
+        this.inputButton.addEventListener("click", (event) => this.addBooks(event, bookList));
+        this.displayBooks();
 
-    this.listLink = document.getElementById('list');
-    this.addLink = document.getElementById('new');
-    this.contactLink = document.getElementById('contact');
+        this.listLink = document.getElementById("list");
+        this.addLink = document.getElementById("new");
+        this.contactLink = document.getElementById("contact");
 
-    this.listLink.addEventListener('click', () => this.showSection('div-list'));
-    this.addLink.addEventListener('click', () => this.showSection('form-section'));
-    this.contactLink.addEventListener('click', () => this.showSection('contact-section'));
+        this.listLink.addEventListener("click", () => this.showSection("div-list"));
+        this.addLink.addEventListener("click", () => this.showSection("form-section"));
+        this.contactLink.addEventListener("click", () => this.showSection("contact-section"));
 
-    this.displayDate('date');
-  }
+        this.displayDate("date");
+    }
+
+    displayBooks = () => {
+        if (!this.bookList) return;
+
+        this.bookList.innerHTML = "";
+        const list = document.createElement("ul");
+        list.classList.add("list-container");
+
+        for (let i = 0; i < this.books.length; i += 1) {
+            const book = this.books[i];
+
+            const listItem = document.createElement("li");
+
+            const booksInfo = document.createElement("span");
+            booksInfo.textContent = `${book.title} by ${book.author}`;
+            listItem.classList.add(i % 2 === 0 ? "even" : "odd");
+            listItem.appendChild(booksInfo);
+            const removeButton = document.createElement("button");
+            removeButton.id = `remove-button-${this.books[i].id}`;
+
+            removeButton.textContent = "remove";
+            removeButton.setAttribute("data-index", i);
+
+            listItem.appendChild(removeButton);
+            list.appendChild(listItem);
+        }
+        this.bookList.appendChild(list);
+        this.attachRemoveButtonListeners();
+    };
 
     attachRemoveButtonListeners = () => {
-      const removeButtons = document.querySelectorAll("button[id^='remove-button-']");
-      removeButtons.forEach((button) => {
-        button.addEventListener('click', this.removeBook.bind(this));
-      });
+        const removeButtons = document.querySelectorAll("button[id^='remove-button-']");
+        removeButtons.forEach((button) => {
+            button.addEventListener("click", this.removeBook.bind(this));
+        });
     };
 
     displayDate = () => {
-      displayDate('date');
+        displayDate("date");
     };
 
     removeBook = (event) => {
-      const bookIndex = event.target.dataset.index;
-      this.books.splice(bookIndex, 1);
-      localStorage.setItem('book', JSON.stringify(this.books));
-      this.displayBooks();
+        const bookIndex = event.target.dataset.index;
+        this.books.splice(bookIndex, 1);
+        localStorage.setItem("book", JSON.stringify(this.books));
+        this.displayBooks();
     };
-
-    displayBooks = displayBooks.bind(this);
-
-    displayBooks = displayBooks.bind(null, this.bookList);
 
     addBooks = addBooks.bind(this);
 
--- modules/addBooks.js
+++ modules/addBooks.js
@@ -1,17 +1,17 @@
-import Book from './books.js';
+import Book from "./books.js";
 
-const addBooks = (event) => {
-  event.preventDefault();
-  const title = this.inputTitle.value;
-  const author = this.inputAuthor.value;
-  if (title !== '' && author !== '') {
-    const book = new Book(Date.now(), title, author);
-    this.books = [book, ...this.books];
-    localStorage.setItem('book', JSON.stringify(this.books));
-  }
-  this.displayBooks();
-  this.inputAuthor.value = '';
-  this.inputTitle.value = '';
+const addBooks = (event, bookList) => {
+    event.preventDefault();
+    const title = bookList.inputTitle.value;
+    const author = bookList.inputAuthor.value;
+    if (title !== "" && author !== "") {
+        const book = new Book(Date.now(), title, author);
+        bookList.books = [book, ...bookList.books];
+        localStorage.setItem("book", JSON.stringify(bookList.books));
+    }
+    bookList.displayBooks();
+    bookList.inputAuthor.value = "";
+    bookList.inputTitle.value = "";
 };
 
 export default addBooks;
