1a35e11dc790c72cfb11a2ca2d030f2e80705080
HENOK MEKONNEN <Henokme@ethiopianairlines.com>
2023-06-07 01:00:46 +0300

    fixed linter errors

--- modules/dateDisplay.js
+++ modules/dateDisplay.js
@@ -1,17 +1,17 @@
-import { DateTime } from "./luxon.js";
+import { DateTime } from './luxon.js';
 
 const displayDate = (elementId) => {
-    const dateElement = document.getElementById(elementId);
+  const dateElement = document.getElementById(elementId);
 
-    const updateTime = () => {
-        const today = DateTime.now();
-        const format = today.toLocaleString(DateTime.DATETIME_FULL);
+  const updateTime = () => {
+    const today = DateTime.now();
+    const format = today.toLocaleString(DateTime.DATETIME_FULL);
 
-        dateElement.textContent = format;
-    };
+    dateElement.textContent = format;
+  };
 
-    updateTime();
-    setInterval(updateTime, 1000);
+  updateTime();
+  setInterval(updateTime, 1000);
 };
 
 export default displayDate;
